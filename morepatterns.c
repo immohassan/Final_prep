037591b1dfeabf6dd8f4205690250fdc22a9cb11
Immohassan <f21bb126@ibitpu.edu.pk>
2022-10-05 06:12:45 +0500

    new algo

--- morepatterns.c
+++ morepatterns.c
@@ -2,154 +2,96 @@
 #include <stdio.h>
 void hollowreversepyramid(int size)
 {
-    int i,j;
-     /* Iterate through rows */
-    for(i=1; i<=size; i++)
+    int row, col;
+    for ( row = size; row >= 1; row--)
     {
-        /* Print leading spaces */
-        for(j=1; j<i; j++)
+        for ( col = 1; col <= 2*size-1; col++)
         {
-            printf(" ");
-        }
-
-        /* Print hollow pyramid */
-        for(j=1; j<=(size*2 - (2*i-1)); j++)
-        {
-            /*
-             * Print star for first row(i==1),
-             * ith column (j==1) and for
-             * last column (size*2-(2*i-1))
-             */ 
-            if(i==1 || j==1 || j==(size*2 - (2*i - 1)))
-            {
+            if (row == size || row+col == size+1 || col == size+row-1){
                 printf("*");
             }
-            else
-            {
+            else{
                 printf(" ");
             }
         }
-
-        /* Move to next line */
         printf("\n");
+        
     }
-
+    
 }
 
 void hollowpyramid(int size)
 {
-    int i,j;
-    for(i=1; i<=size; i++)
+    int row, col;
+    for ( row = 1; row <= size; row++)
     {
-        /* Print trailing spaces */
-        for(j=i; j<size; j++)
+        for ( col = 1; col <= 2*size-1; col++)
         {
-            printf(" ");
-        }
-
-        /* Print hollow pyramid */
-        for(j=1; j<=(2*i-1); j++)
-        {
-            /*
-             * Print star for last row (i==size),
-             * first column(j==1) and for
-             * last column (j==(2*i-1)).
-             */ 
-            if(i==size || j==1 || j==(2*i-1))
-            {
+            if (row == size || row+col == size+1 || col == size+row-1){
                 printf("*");
             }
-            else
-            {
+            else{
                 printf(" ");
             }
         }
-
-        /* Move to next line */
         printf("\n");
+        
     }
+    
 }
 
 void hollowdiamond(int size)
 {
-    int i,j;
-    for(i=1; i<=size; i++)
+    int row, col;
+    for ( row = 1; row <= size; row++)
     {
-        /* Print trailing spaces */
-        for(j=i; j<size; j++)
+        for ( col = 1; col <= 2*size-1; col++)
         {
-            printf(" ");
-        }
-
-        /* Print hollow pyramid */
-        for(j=1; j<=(2*i-1); j++)
-        {
-            /*
-             * Print star for last row (i==size),
-             * first column(j==1) and for
-             * last column (j==(2*i-1)).
-             */ 
-            if( j==1 || j==(2*i-1))
-            {
+            if ( row+col == size+1 || col == size+row-1){
                 printf("*");
             }
-            else
-            {
+            else{
                 printf(" ");
             }
         }
-
-        /* Move to next line */
         printf("\n");
+        
     }
-
-    for(i=1; i<=size; i++)
+    //downwards
+    for ( row = size; row >= 1; row--)
     {
-        /* Print leading spaces */
-        for(j=1; j<i; j++)
+        for ( col = 1; col <= 2*size-1; col++)
         {
-            printf(" ");
-        }
-
-        /* Print hollow pyramid */
-        for(j=1; j<=(size*2 - (2*i-1)); j++)
-        {
-            /*
-             * Print star for first row(i==1),
-             * ith column (j==1) and for
-             * last column (size*2-(2*i-1))
-             */ 
-            if(j==1 || j==(size*2 - (2*i - 1)))
-            {
+            if ( row+col == size+1 || col == size+row-1){
                 printf("*");
             }
-            else
-            {
+            else{
                 printf(" ");
             }
         }
-
-        /* Move to next line */
         printf("\n");
+        
     }
+    
 }
 
-void display(int r){
-    int x,y;
+void display(int r)
+{
+    int x, y;
     int xsq, ysq;
-    int rsq = r*r;
-    // x and y representing coordinates on an imaginary cartisian plane 
-    for(x=-r;x<=r;x++)
-    {  
-        xsq = x*x;
-        printf("\t\t\t");//some tabs to make it kinda "centered"
-        for(y=-r;y<=r;y++)
+    int rsq = r * r;
+    // x and y representing coordinates on an imaginary cartisian plane
+    for (x = -r; x <= r; x++)
+    {
+        xsq = x * x;
+        printf("\t\t\t"); // some tabs to make it kinda "centered"
+        for (y = -r; y <= r; y++)
         {
-            /*if the coordinate:(x,y) is a point of the equation x^2+y^2=r^2 prints the char:*, if 
+            /*if the coordinate:(x,y) is a point of the equation x^2+y^2=r^2 prints the char:*, if
             not leaves that space empty*/
-            int ysq = y*y;
+            int ysq = y * y;
             int dsq = xsq + ysq;
-            if( dsq > rsq - r && dsq < rsq + r)
+            if (dsq > rsq - r && dsq < rsq + r)
                 printf("*");
             else
                 printf(" ");
@@ -157,15 +99,17 @@ void display(int r){
         printf("\n");
     }
 }
+
 
 int main()
 {
     int size;
     printf("Enter size: ");
     scanf("%d", &size);
-    // hollowpyramid(size);
-    // hollowreversepyramid(size);
+    hollowpyramid(size);
+    hollowreversepyramid(size);
     hollowdiamond(size);
+    // blah(size);
     // display(size);
     return 0;
 }
